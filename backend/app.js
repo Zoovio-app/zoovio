536beb365137ab2d6934437bbc61ce2862aed432
dannyl <danilovlaurent@pursuit.org>
2020-07-10 17:11:44 -0400

    create backend port

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,9 @@
-// ------BACK END--------//
+const express = require("express");
+const cors = require("cors");
+const port = 3001;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`LISTENING TO PORT ${port}`);
+});
