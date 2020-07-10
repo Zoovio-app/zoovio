008893fe365bd0dd5ba05f377456b6b6249501e6
dannyl <danilovlaurent@pursuit.org>
2020-07-10 17:22:26 -0400

    create database

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const port = 3001;
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`LISTENING TO PORT ${port}`);
---
+++ backend/database/index.js
@@ -0,0 +1,3 @@
+require("dotenv").config();
+const pgp = require("pg-promise")({});
+const db = pgp(process.env.DATABASE_URL);
