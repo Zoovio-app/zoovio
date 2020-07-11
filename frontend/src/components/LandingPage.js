7a221bee9cd337808023a4af3973eb6074cc1fc2
Ohidurr <OhidurRahman@pursuit.org>
2020-07-11 11:47:21 -0400

    remove url and sample session storage

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -21,13 +21,12 @@ const LandingPage = () => {
     const handleLogin = async(e) => {
         e.preventDefault()
         let display_name = e.target.display_name.value
-        let url=`http://localhost:3001/users/search/${display_name}`
+        
         try{
-        let res = await axios.get(url)
+        let res = await axios.get()
         let user=res.data.body.searchUser
             if(user.length){
-                sessionStorage.setItem("currentUser",user[0].id)
-                history.push(`/feed`)
+                history.push()
             }else{
                 alert("fail")
             }
