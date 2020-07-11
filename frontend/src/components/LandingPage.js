103c471eb6a98a252b0f8b026127e85d73c16021
dannyl <danilovlaurent@pursuit.org>
2020-07-11 18:50:46 -0400

    create controled inputs for email and password

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,63 +1,33 @@
 import React, { useState } from "react";
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
-
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 const LandingPage = () => {
-    const [displyName, setDisplyName] = useState("");
-    const [Form,setShowForm] = useState(false)
-    const [signUpForm, setsignUpForm] = useState(false)
-    const history = useHistory()
-    
+  const [displyName, setDisplyName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
 
-  const handleLogInClick = () =>  {
-        console.log(Form)
-        setShowForm(!Form)
-    }
-    const handleSignUpClick = () =>  {
-        console.log(signUpForm)
-        setsignUpForm(!signUpForm)
-    }
-    const handleLogin = async(e) => {
-        e.preventDefault()
-        let display_name = e.target.display_name.value
-        
-        try{
-        let res = await axios.get()
-        let user=res.data.body.searchUser
-            if(user.length){
-                history.push()
-            }else{
-                alert("fail")
-            }
-        }catch(error){
-            console.log(error);
-        }
-    }
+  return (
+    <div className="login-info">
+      <h1>ZooVio</h1>
+      <form id="loginForm">
+        <input
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="email"
+        ></input>
+        <input
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="password"
+        ></input>
+        <button type="submit">submit</button>
+      </form>
+      <p>Don't have an account? get started!</p>{" "}
+    </div>
+  );
+};
 
-
-  const login = ()=>{
-        return <div className="login-info">
-            <form id="loginForm" onSubmit={handleLogin}>
-                <input name="display_name" placeholder="username"></input>
-                {/* <input name="password" type="password" placeholder="password"></input> */}
-                <button type="submit">submit</button>
-            </form>
-        
-        </div>
-    }
-    return(
-        <div>
-        <div id="rightBanner">
-         <button id="logIN" onClick={handleLogInClick}>Login</button>
-         <p>Don't have an account? get started!</p> <button id="signUp">Signup</button>
-
-        </div>
-        <h1>ZooVio</h1>
-         {Form?login():null}
-        {/* {signUpForm?signup():null} */}
-        </div>
-    )
-}
-
-export default LandingPage
+export default LandingPage;
