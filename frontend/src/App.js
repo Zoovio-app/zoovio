1b3674118be5513327c5f86d767b1759342cc4ad
Ohidurr <OhidurRahman@pursuit.org>
2020-07-11 11:45:29 -0400

    add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom'
 import "./App.css";
+import LandingPage from './components/LandingPage'
 
 function App() {
-  return <div className="App"></div>;
+  return(
+
+  <div className="App">
+  <Switch>
+    <Route exact path={"/"}>
+      <LandingPage/>
+    </Route>
+    
+  </Switch>
+
+  </div>
+  );
 }
 
 export default App;
+
