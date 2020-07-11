530f1807aebbf35e83aa71fb4843cf9c764fad6e
dannyl <danilovlaurent@pursuit.org>
2020-07-11 19:08:24 -0400

    create login and log out firebase functions

---
+++ frontend/src/util/firebaseFunctions.js
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+
+export const login = (email, password) =>
+  firebase.auth().signInWithEmailAndPassword(email, password);
+
+export const logOut = () => firebase.auth().signOut();
+
+export const signUp = (email, password) =>
+  firebase.auth().createUserWithEmailAndPassword(email, password);
