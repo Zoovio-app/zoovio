74fa91879ea7ca6ba473622e29838793ac4b9b04
Ohidurr <OhidurRahman@pursuit.org>
2020-07-11 11:21:50 -0400

    add login and signup bottons

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -18,15 +18,35 @@ const LandingPage = () => {
         console.log(signUpForm)
         setsignUpForm(!signUpForm)
     }
-    const Login = () => {
+    const HandleLogin = () => {
 
     }
 
+  const login = ()=>{
+        return <div className="login-info">
+            <form id="loginForm" onSubmit={handleLogin}>
+                <input name="display_name" placeholder="username"></input>
+                {/* <input name="password" type="password" placeholder="password"></input> */}
+                <button type="submit">submit</button>
+            </form>
+        
+        </div>
+    }
     return(
+        <div>
+        <div id="rightBanner">
+         <button id="logIN" onClick={handleLogInClick}>Login</button>
+         <p>Don't have an account? get started!</p> <button id="signUp">Signup</button>
+
+        </div>
+        
         <form>
         
 
         </form>
+         {Form?login():null}
+        {signUpForm?signup():null}
+        </div>
     )
 }
 
