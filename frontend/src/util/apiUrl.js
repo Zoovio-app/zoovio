c847202461d49bafa688984c5cfbe9d9cb5e8ea4
dannyl <danilovlaurent@pursuit.org>
2020-07-14 14:30:13 -0400

    commit

--- frontend/src/util/apiUrl.js
+++ frontend/src/util/apiUrl.js
@@ -1,5 +1,5 @@
 export const apiUrl = () => {
   return window.location.hostname === "localhost"
     ? "http://localhost:3001"
-    : "https://zoovio.herokuapp.com/";
+    : "https://zoovio.herokuapp.com";
 };
