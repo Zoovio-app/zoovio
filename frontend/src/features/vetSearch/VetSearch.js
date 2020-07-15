823dd3813ea35d49769f9e2c5027cc8b1f1ea750
Ohidurr <OhidurRahman@pursuit.org>
2020-07-14 21:31:35 -0400

    add test selector

--- frontend/src/features/vetSearch/VetSearch.js
+++ frontend/src/features/vetSearch/VetSearch.js
@@ -1,1 +1,20 @@
 import React from 'react'
+
+const Search = () => {
+
+ return(
+
+     <div>
+     <select>
+        <option>Dermatologist</option> <input></input>
+     </select>
+     </div>
+
+
+ )
+}
+
+export default Search 
+
+// Google Place API: https://developers.google.com/places/web-service/search
+// google maps API: https://developers.google.com/maps/documentation/javascript/tutorial
