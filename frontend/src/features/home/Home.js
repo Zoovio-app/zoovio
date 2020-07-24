c2de3492f55b07a962a62ebc17a9f0d7678387cd
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-07-24 10:55:35 -0400

    create tasks component

--- frontend/src/features/home/Home.js
+++ frontend/src/features/home/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState, useLayoutEffect } from "react";
+import React, { useContext, useState, useLayoutEffect } from "react";
 import { logOut } from "../../util/firebaseFunctions";
 import axios from "axios";
 import { apiUrl } from "../../util/apiUrl";
@@ -9,6 +9,7 @@ import {
   userInfoState,
   clearUserInfo,
 } from "../userInfo/userInfoSlice";
+import Tasks from "../tasks/Tasks";
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -50,7 +51,7 @@ const Home = () => {
   ) : (
     <div>
       <h1>hi,{state.user ? state.user.name : null} </h1>
-
+      <Tasks/>
       <button onClick={signOut}> Log Out</button>
     </div>
   );
