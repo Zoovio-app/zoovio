3814fd763fdd60a7ae6feea63eca7a2f4cde4745
Ohidurr <OhidurRahman@pursuit.org>
2020-07-26 16:48:42 -0400

    add google api and google script

--- frontend/src/features/search/VetSearch.js
+++ frontend/src/features/search/VetSearch.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api"
 
-
+const libaries = ['places']
+const mapContainerStyle = {
+    width: '100vw',
+    height: '100vh'
+}
+const center = {
+    lat: 40.7128,
+    lng: 74.0060
+}
 const VetSearch = () =>{
 
+    const {isLoaded,loadError} = useLoadScript({
+        googleMapsApiLey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libaries
+    })
+    if(loadError) return "Error loading maps"
+    // if(!isloaded) return "Loading maps"
     return(
         <div>
             <p> Hello!</p>
+            <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8}></GoogleMap>
         </div>
     )
 }
