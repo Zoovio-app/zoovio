d73ab36342b3781f36144381a5adfb43a2bfa8b2
dannyl <danilovlaurent@pursuit.org>
2020-07-26 20:14:43 -0400

    create add check token function to get all tasks by day rout

--- backend/routes/tasks/tasks.js
+++ backend/routes/tasks/tasks.js
@@ -13,6 +13,6 @@ tasks.get("/pet/:id", getAllTasksByPet);
 tasks.post("/", createNewTask);
 tasks.get("/:id", getAllTasksByUser);
 tasks.get("/month/:month", getAllTasksByMonth);
-tasks.get("/day/:day", getAllTasksByDay);
+tasks.get("/day/:day", checkToken, getAllTasksByDay);
 
 module.exports = tasks;
