5e466412e6ef1612371a7855e6ef8fcad4de2113
dannyl <danilovlaurent@pursuit.org>
2020-07-29 11:00:59 -0400

    refactor helper function

--- frontend/src/features/calendar/helpers/helpers.js
+++ frontend/src/features/calendar/helpers/helpers.js
@@ -1,22 +1,16 @@
 import React from "react";
 
 export const taskDatesArr = (tasks) => {
-  let count = new Set();
-  // eslint-disable-next-line
-  return tasks.map((task) => {
-    let value = new Date(task.due_date).getDate();
-    if (!count.has(value)) {
-      count.add(value);
-      return value;
-    }
-  });
+  let count = new Set(tasks.map((task) => new Date(task.due_date).getDate()));
+  return [...count];
 };
 
 export const func = (date, dates, view) => {
-  // eslint-disable-next-line
   return dates.map((element, i) => {
     if (view === "month" && date.getDate() === element) {
       return <div key={i} className="dotDiv"></div>;
+    } else {
+      return null;
     }
   });
 };
