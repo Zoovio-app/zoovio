65334c00cb0b00e44bbda9f8c8f99d88ed1f1504
Ohidurr <OhidurRahman@pursuit.org>
2020-07-28 21:25:40 -0400

    add maps, add map styling, add proper npm i stuff, map is loading

--- frontend/src/features/search/VetSearch.js
+++ frontend/src/features/search/VetSearch.js
@@ -1,30 +1,62 @@
 import React from 'react'
 import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api"
-
+import mapStyle from "./mapStyle"
 const libaries = ['places']
+
 const mapContainerStyle = {
     width: '100vw',
-    height: '100vh'
+    height: '60vh'
 }
 const center = {
-    lat: 40.7128,
-    lng: 74.0060
+    lat: 40.71427,
+    lng: -74.00597
 }
-const VetSearch = () =>{
+const options = {
+    styles: mapStyle,
+    
+}
 
-    const {isLoaded,loadError} = useLoadScript({
-        googleMapsApiLey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-        libaries
-    })
-    if(loadError) return "Error loading maps"
-    // if(!isloaded) return "Loading maps"
+  const VetSearch = () =>{
+
+     const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libaries,
+    });
+    if(loadError) return "Error loading maps";
+    if(!isLoaded) return "Loading maps";
+    
     return(
         <div>
-            <p> Hello!</p>
-            <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8}></GoogleMap>
+        <GoogleMap mapContainerStyle={mapContainerStyle} zoom={7} center={center}
+        options={options}>
+
+        </GoogleMap>
         </div>
     )
-}
+  }
+
+
 
 
 export default VetSearch
+
+// import React from 'react'
+// import { Map, GoogleApiWrapper } from 'google-maps-react'
+
+// const VetSearch = () => {
+
+//     return(
+//         <Map
+//         google={this.props.google}
+//         zoom={7}
+//         style={mapStyles}
+//         initialCenter = {{lat: 40.7128, lng: 74.0060}}
+//         />
+//     )
+// }
+
+// export default VetSearch ({
+//     apikey: REACT_APP_GOOGLE_MAPS_API_KEY
+// }) 
+
+
