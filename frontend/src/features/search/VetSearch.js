a3d14bf6e16c5975f764448e6be940107f06f048
Ohidurr <OhidurRahman@pursuit.org>
2020-07-29 01:24:45 -0400

    search bar not disabled

--- frontend/src/features/search/VetSearch.js
+++ frontend/src/features/search/VetSearch.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useRef }  from 'react'
 import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api"
-import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete"
+import usePlacesAutocomplete, { getGeocode, getLatLng, geocodeByAddress } from "use-places-autocomplete"
 import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from "@reach/combobox"
 import "@reach/combobox/styles.css"
 import mapStyle from "./mapStyle"
@@ -21,7 +21,7 @@ const options = {
     
 }
 
-  const VetSearch = () =>{
+  const VetSearch = () => {
 
      const { isLoaded, loadError } = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -43,11 +43,12 @@ const options = {
         const {ready, value, suggestions: {status, data}, setValue, clearSuggestions } = usePlacesAutocomplete ({
             requestOptions: {
                 location: {lat: () => 40.71427, lng:() => -74.00597 },
-                radius: 300 * 1000
-            }
+                radius: 100 * 1000
+            },
+
         })
         return(
-            <div>
+            <div className="search">
             <Combobox onSelect = { async (address) => {
                 try{
                     const results = await getGeocode({address})
@@ -63,12 +64,12 @@ const options = {
             <ComboboxInput value={value} onChange={(e) => {
                 setValue(e.target.value)
             }}
-            disabled={!ready}
+            // disabled={!ready}
             placeholder="Search for a Vet"
             />
             <ComboboxPopover>
             <ComboboxList>
-                {status === "Success" && data.map(({id, description}) =>(
+                {status === "OK" && data.map(({id, description}) =>(
                     <ComboboxOption key={id} value={description} />
                 ))}
                 </ComboboxList>
