0f3a2b49d0f276121284cf46a7cae9aa6ea3543d
dannyl <danilovlaurent@pursuit.org>
2020-08-21 17:13:04 -0400

    create styles for navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,53 +19,79 @@ function App() {
   return (
     <AuthProvider>
       <div className="App">
-        <ProtectedRoute>
-          <Nav />
-        </ProtectedRoute>
-        <Switch>
-          <AuthRoute exact path="/login">
-            <LandingPage />
-          </AuthRoute>
-          <ProtectedRoute exact path="/pets/create">
-            <PetForm />
+        <div
+          style={{
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            // justifyContent: "spaceBetween",
+          }}
+        >
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+              <path
+                fill="#f16643"
+                fill-opacity="1"
+                d="M0,192L60,181.3C120,171,240,149,360,122.7C480,96,600,64,720,53.3C840,43,960,53,1080,69.3C1200,85,1320,107,1380,117.3L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+              ></path>
+            </svg>
+          </div>
+
+          <Switch>
+            <AuthRoute exact path="/login">
+              <LandingPage />
+            </AuthRoute>
+            <ProtectedRoute exact path="/pets/create">
+              <PetForm />
+            </ProtectedRoute>
+            <div
+              style={{
+                marginTop: " -275px",
+                display: "flex",
+                justifyContent: "center",
+                height: "60vh",
+              }}
+            >
+              <AnimatePresence exitBeforeEnter>
+                <Switch location={location} key={location.pathname}>
+                  <ProtectedRoute exact path="/home">
+                    <Home />
+                  </ProtectedRoute>
+
+                  <ProtectedRoute exact path="/pets">
+                    <Pets />
+                  </ProtectedRoute>
+
+                  <ProtectedRoute exact path="/calendar/tasks/:day">
+                    <Tasks />
+                  </ProtectedRoute>
+
+                  <ProtectedRoute exact path="/tasks/create">
+                    <TaskForm />
+                  </ProtectedRoute>
+
+                  <ProtectedRoute exact path="/calendar">
+                    <CalendarPage />
+                  </ProtectedRoute>
+                </Switch>
+              </AnimatePresence>
+            </div>
+            <AuthRoute exact path="/">
+              <LandingPage />
+            </AuthRoute>
+
+            <AuthRoute exact path="/signup">
+              <SignUp />
+            </AuthRoute>
+
+            <AuthRoute path="*">
+              <LandingPage />
+            </AuthRoute>
+          </Switch>
+          <ProtectedRoute>
+            <Nav />
           </ProtectedRoute>
-
-          <AnimatePresence exitBeforeEnter>
-            <Switch location={location} key={location.pathname}>
-              <ProtectedRoute exact path="/home">
-                <Home />
-              </ProtectedRoute>
-
-              <ProtectedRoute exact path="/pets">
-                <Pets />
-              </ProtectedRoute>
-
-              <ProtectedRoute exact path="/calendar/tasks/:day">
-                <Tasks />
-              </ProtectedRoute>
-
-              <ProtectedRoute exact path="/tasks/create">
-                <TaskForm />
-              </ProtectedRoute>
-
-              <ProtectedRoute exact path="/calendar">
-                <CalendarPage />
-              </ProtectedRoute>
-            </Switch>
-          </AnimatePresence>
-
-          <AuthRoute exact path="/">
-            <LandingPage />
-          </AuthRoute>
-
-          <AuthRoute exact path="/signup">
-            <SignUp />
-          </AuthRoute>
-
-          <AuthRoute path="*">
-            <LandingPage />
-          </AuthRoute>
-        </Switch>
+        </div>
       </div>
     </AuthProvider>
   );
--- frontend/src/features/navBar/Nav.js
+++ frontend/src/features/navBar/Nav.js
@@ -1,14 +1,51 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./css/nav.css";
+import pet from "../../images/paw.png";
+import home from "../../images/browser.png";
+import calendar from "../../images/appointment.png";
+import vet from "../../images/vet.png";
 
 const Nav = () => {
   return (
-    <div style={{ display: "flex", flexDirection: " column" }}>
-      <NavLink to="/home">HOME</NavLink>
+    <div>
+      <div className="nav">
+        <div
+          style={{ borderBottomLeftRadius: "20px" }}
+          className="navLinksCont"
+        >
+          <NavLink className="link" to="/home">
+            <div className="navImgCont">
+              <img alt="" src={home} />
+            </div>
+          </NavLink>
+        </div>
+        <div className="navLinksCont">
+          <NavLink className="link" to="/pets">
+            <div className="navImgCont">
+              <img alt="" src={pet} />
+            </div>
+          </NavLink>
+        </div>
 
-      <NavLink to="/calendar">CALENDAR</NavLink>
-
-      <NavLink to="/pets">PETS</NavLink>
+        <div className="navLinksCont">
+          <NavLink className="link" to="/calendar">
+            <div className="navImgCont">
+              <img alt="" src={calendar} />
+            </div>
+          </NavLink>
+        </div>
+        <div
+          style={{ borderBottomRightRadius: "20px" }}
+          className="navLinksCont"
+        >
+          <NavLink className="link" to="/calendar">
+            <div className="navImgCont">
+              <img alt="" src={vet} />
+            </div>
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 };
