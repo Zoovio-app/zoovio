5a0f12c47155ef6f0872ccedb72f6036aadeb2b2
dannyl <danilovlaurent@pursuit.org>
2020-08-26 15:52:04 -0400

    complete splash page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, useLocation } from "react-router-dom";
+import { Switch, useLocation, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./features/landingPage/LandingPage";
 import { AuthRoute, ProtectedRoute } from "./util/routeUtil";
@@ -13,45 +13,35 @@ import Tasks from "./features/calendar/tasks/Tasks";
 import TaskForm from "./features/taskForm/TaskForm";
 import Pets from "./features/pets/Pets";
 import PetForm from "./features/petForm/PetForm";
+import SvgWave from "./features/svgWaves/SvgWave";
+import SplashPage from "./features/splashpage/SplashPage";
 
 function App() {
   const location = useLocation();
   return (
     <AuthProvider>
       <div className="App">
-        <div
-          style={{
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            // justifyContent: "spaceBetween",
-          }}
-        >
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-              <path
-                fill="#f16643"
-                fill-opacity="1"
-                d="M0,192L60,181.3C120,171,240,149,360,122.7C480,96,600,64,720,53.3C840,43,960,53,1080,69.3C1200,85,1320,107,1380,117.3L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
-              ></path>
-            </svg>
-          </div>
+        <div className="appCont">
+          <Route path={["/home"]}>
+            <SvgWave />
+          </Route>
 
           <Switch>
+            <AuthRoute exact path="/">
+              <SplashPage />
+            </AuthRoute>
+
+            <AuthRoute exact path="/signup">
+              <SignUp />
+            </AuthRoute>
+
             <AuthRoute exact path="/login">
               <LandingPage />
             </AuthRoute>
             <ProtectedRoute exact path="/pets/create">
               <PetForm />
             </ProtectedRoute>
-            <div
-              style={{
-                marginTop: " -275px",
-                display: "flex",
-                justifyContent: "center",
-                height: "60vh",
-              }}
-            >
+            <div className="inAppContent">
               <AnimatePresence exitBeforeEnter>
                 <Switch location={location} key={location.pathname}>
                   <ProtectedRoute exact path="/home">
@@ -73,24 +63,21 @@ function App() {
                   <ProtectedRoute exact path="/calendar">
                     <CalendarPage />
                   </ProtectedRoute>
+
+                  <AuthRoute path="*">
+                    <LandingPage />
+                  </AuthRoute>
                 </Switch>
               </AnimatePresence>
             </div>
-            <AuthRoute exact path="/">
-              <LandingPage />
-            </AuthRoute>
-
-            <AuthRoute exact path="/signup">
-              <SignUp />
-            </AuthRoute>
 
             <AuthRoute path="*">
               <LandingPage />
             </AuthRoute>
           </Switch>
-          <ProtectedRoute>
+          <Route path={["/home", "/pets", "/calendar"]}>
             <Nav />
-          </ProtectedRoute>
+          </Route>
         </div>
       </div>
     </AuthProvider>
--- frontend/src/features/splashpage/SplashPage.js
+++ frontend/src/features/splashpage/SplashPage.js
@@ -1,10 +1,145 @@
-import React from 'react';
-
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import vet from "../../images/vetpic.png";
+import relax from "../../images/relax.png";
+import hangout from "../../images/hangout.png";
+// import delivery from "../../images/delivery.png";
+import "./css/splash.css";
+import { Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SplashPage = () => {
-    return (
-        
-    );
-}
+  const history = useHistory();
+  return (
+    <div className="splashMain">
+      <div className="topCont">
+        <div className="titleCont">
+          <h1 className="titleE">ZooVio</h1>
+        </div>
+        <div>
+          <svg
+            className="blob"
+            viewBox="0 0 200 200"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#F3BFD0"
+              d="M47.3,-64.1C62.9,-53.8,78.4,-42.1,77.8,-28.8C77.2,-15.5,60.6,-0.6,50.9,12.6C41.2,25.9,38.4,37.5,31.1,45.1C23.8,52.8,11.9,56.5,-0.5,57.2C-12.9,58,-25.9,55.7,-40.2,50.3C-54.5,44.9,-70.2,36.4,-70.2,25.4C-70.3,14.3,-54.7,0.8,-45.5,-10.9C-36.3,-22.6,-33.5,-32.4,-27,-45.7C-20.4,-59,-10.2,-75.7,2.8,-79.6C15.8,-83.5,31.7,-74.4,47.3,-64.1Z"
+              transform="translate(100 100)"
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          height: "60vh",
+          alignItems: "center",
+        }}
+      >
+        <div className="blob2">
+          <svg
+            className="blob"
+            viewBox="0 0 200 200"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#F3BFD0"
+              d="M47.3,-64.1C62.9,-53.8,78.4,-42.1,77.8,-28.8C77.2,-15.5,60.6,-0.6,50.9,12.6C41.2,25.9,38.4,37.5,31.1,45.1C23.8,52.8,11.9,56.5,-0.5,57.2C-12.9,58,-25.9,55.7,-40.2,50.3C-54.5,44.9,-70.2,36.4,-70.2,25.4C-70.3,14.3,-54.7,0.8,-45.5,-10.9C-36.3,-22.6,-33.5,-32.4,-27,-45.7C-20.4,-59,-10.2,-75.7,2.8,-79.6C15.8,-83.5,31.7,-74.4,47.3,-64.1Z"
+              transform="translate(100 100)"
+            />
+          </svg>
+        </div>
+        <Carousel>
+          <Carousel.Item>
+            <img src={hangout} alt="First slide" />
+
+            <Carousel.Caption>
+              <div>
+                <h3
+                  style={{
+                    color: "#141827",
+                    fontWeight: "700",
+                    marginBottom: "1.25rem",
+                    marginTop: "1.25rem",
+                  }}
+                >
+                  {" "}
+                  Create Tasks
+                </h3>
+                <p
+                  style={{
+                    color: "#225095",
+                    fontSize: "20px",
+                    fontWeight: "500",
+                  }}
+                >
+                  keep track of your pets daily activities.
+                </p>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img src={vet} alt="" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={relax} alt="" />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+      <div className="bottomCont">
+        <div className="blob4">
+          <svg
+            className="blob"
+            viewBox="0 0 200 200"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#F3BFD0"
+              d="M47.3,-64.1C62.9,-53.8,78.4,-42.1,77.8,-28.8C77.2,-15.5,60.6,-0.6,50.9,12.6C41.2,25.9,38.4,37.5,31.1,45.1C23.8,52.8,11.9,56.5,-0.5,57.2C-12.9,58,-25.9,55.7,-40.2,50.3C-54.5,44.9,-70.2,36.4,-70.2,25.4C-70.3,14.3,-54.7,0.8,-45.5,-10.9C-36.3,-22.6,-33.5,-32.4,-27,-45.7C-20.4,-59,-10.2,-75.7,2.8,-79.6C15.8,-83.5,31.7,-74.4,47.3,-64.1Z"
+              transform="translate(100 100)"
+            />
+          </svg>
+        </div>
+        <div className="splashButtonsCont">
+          <div className="buttsHolder">
+            <div className="splashSignUpCont">
+              <Button onClick={() => history.push("/signup")}>Sign Up</Button>
+            </div>
+            <p style={{ padding: "1px" }}>Already a user?</p>{" "}
+            <Link exact to="/login">
+              Login
+            </Link>
+          </div>
+
+          <div className="footerDiv">
+            <Link>About</Link>
+            <Link>Faq</Link>
+            <Link>Other</Link>
+          </div>
+        </div>
+
+        <div className="blob3">
+          <svg
+            className="blob"
+            viewBox="0 0 200 200"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#F3BFD0"
+              d="M47.3,-64.1C62.9,-53.8,78.4,-42.1,77.8,-28.8C77.2,-15.5,60.6,-0.6,50.9,12.6C41.2,25.9,38.4,37.5,31.1,45.1C23.8,52.8,11.9,56.5,-0.5,57.2C-12.9,58,-25.9,55.7,-40.2,50.3C-54.5,44.9,-70.2,36.4,-70.2,25.4C-70.3,14.3,-54.7,0.8,-45.5,-10.9C-36.3,-22.6,-33.5,-32.4,-27,-45.7C-20.4,-59,-10.2,-75.7,2.8,-79.6C15.8,-83.5,31.7,-74.4,47.3,-64.1Z"
+              transform="translate(100 100)"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SplashPage;
