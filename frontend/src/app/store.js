3c773f62135c9beb81175679d39d22649fb6dea1
darsuabasi <abasibasiurua@gmail.com>
2020-08-25 21:54:18 -0400

    add authinfo and messaginginfo reducers

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,13 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import userInfo from "../features/userInfo/userInfoSlice";
+import messagingInfo from '../features/docSRC/slices/messagingInfoSlice';
+import authInfo from '../features/docSRC/slices/authInfoSlice';
 
 const reducer = {
   userInfo,
+  messagingInfo,
+  authInfo
 };
 
 export default configureStore({
