7298339afc19922a3955f794e1a528dd5853b0e6
darsuabasi <abasibasiurua@gmail.com>
2020-09-03 17:40:29 -0400

    doctor navBar css changes with routes and styling

--- frontend/src/features/docSRC/docPortal/DoctorNav/DoctorNav.js
+++ frontend/src/features/docSRC/docPortal/DoctorNav/DoctorNav.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, Link } from 'react-router-dom';
 import { cloudLogout } from '../../../../util/logoutFunctions';
 import './doctorNav.css';
 import { authInfoState } from '../../slices/authInfoSlice';
+import { AuthContext } from '../../../../providers/AuthContext';
 
 
 const DoctorNav = () => {
     const auth = useSelector(authInfoState);
     const dispatch = useDispatch();
+    const { currentUser } = useContext(AuthContext); 
 
 
     const displayNavForDoctor = () => {
-        if(auth.authenticated) {
+        if(currentUser) {
             return( 
                 <div className="user-navBar">
-                    <ul>
-                        <li className="style-navigation"><a class="hoverable" href="/doctor/home"> Home </a></li>
-                        <li className="style-navigation"><a class="hoverable" href="/messaging"> Messages </a></li>
-                        <li className="style-navigation"><a class="hoverable" href="/doctor/appointments"> Upcoming Appointments</a></li>
-                        <li className="style-navigation"><a class="hoverable" href="/doctor/virtual-appointment"> Virtual Appointments </a></li>
-                        <li className="style-navigation"><a class="hoverable" href="/doctor/pet-charts"> Pet Charts </a></li>
+                    <ul className="docPortalNavUl">
+                        <li className="style-navigation-left"><a className="syleA2" class="hoverable" href="/doctor/home"> Home </a></li>
+                        <li className="style-navigation-right"><a className="syleA2" class="hoverable" href="/doctor/pet-charts"> Pet Charts </a></li>
+                        <li className="style-navigation-right"><a className="syleA2" class="hoverable" href="/doctor/virtual-appointment"> Virtual Appointments </a></li>
+                        <li className="style-navigation-right"><a className="syleA2" class="hoverable" href="/doctor/appointments"> Upcoming Appointments</a></li>
+                        <li className="style-navigation-right"><a className="syleA2" class="hoverable" href="/messaging"> Messages </a></li>
                         {/* <li> <Link to={'/'} onClick={() => {dispatch(cloudLogout(auth.uid))}}>Logout</Link> </li> */}
                     </ul>
                 </div>
@@ -30,7 +32,7 @@ const DoctorNav = () => {
                 <div className="non-user-nav">
                     {/* <ul className='pintrestLogoLetters'>P</ul> */}
                     {/* <div className="logo-pin">  */}
-                    <NavLink className="mainPortal" to={"/"}> LOGO </NavLink>
+                    <NavLink className="mainPortal" to={"/doctor/portal"}> LOGO </NavLink>
                     <NavLink className="mainPortal" to={"/about"}> About Zoovio </NavLink>
                     <NavLink className="mainPortal" to={"/contact"}> Contact </NavLink>
                     <NavLink className="mainPortal" id="login" to={"/doctor/login"}> Log in</NavLink>
@@ -52,20 +54,6 @@ const DoctorNav = () => {
         </div>
     )
 }
-
-
-//     return (
-//         <div className="docNavbar">
-//             <ul>
-//                 <li className="style-navigation"><a class="hoverable" href="/doctor/home"> Home </a></li>
-//                 <li className="style-navigation"><a class="hoverable" href="/messaging"> Messages </a></li>
-//                 <li className="style-navigation"><a class="hoverable" href="/doctor/appointments"> Upcoming Appointments</a></li>
-//                 <li className="style-navigation"><a class="hoverable" href="/doctor/virtual-appointment"> Virtual Appointments </a></li>
-//                 <li className="style-navigation"><a class="hoverable" href="/doctor/pet-charts"> Pet Charts </a></li>
-//             </ul>
-//         </div>
-//     )
-// }
 
 export default DoctorNav
 
