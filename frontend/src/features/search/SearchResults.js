9eaff529663392907033edf2b8ebd536cd12d5ae
Ohidurr <ohidurrahman96@gmail.com>
2020-09-06 02:59:57 -0400

    tremove and re-add pkg.json and nmodules

--- frontend/src/features/search/SearchResults.js
+++ frontend/src/features/search/SearchResults.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SingleResult from "./SingleResult";
-import "./CSS/SearchResults.css";
+// import "./CSS/SearchResults.css";
 
 const SearchResults = (props) => {
  
