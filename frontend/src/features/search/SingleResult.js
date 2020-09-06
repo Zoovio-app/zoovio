86ef2ba34b1bd716dca819e4b9b381ee0396b371
Ohidurr <ohidurrahman96@gmail.com>
2020-09-05 22:26:01 -0400

    add search display

---
+++ frontend/src/features/search/SingleResult.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./searchCss/SingleResult.css";
+import PawRating from "./PawRating";
+import styles from "./searchCss/SearchResults.css";
+
+const SingleResult = (props) => {
+ 
+
+  return (
+    <div className="single-result">
+      <div className="card">
+        <img className="image" src={props.result.image_url} alt="image" />
+        <div className="container">
+          <div className="info">
+            <h2 class="subtitle">
+              <strong> {props.result.name} </strong>
+            </h2>
+            <p className="phone">{props.result.display_phone}</p>
+            <p className="address">{props.result.location["address1"]}</p>
+            <p className="address">{props.result.location.city}</p>
+            <p className="address">{props.result.location.zip_code}</p>
+            <PawRating
+              reviewCount={props.result.review_count}
+              rating={props.result.rating}
+            />
+            <p>
+              <span class="tag"></span>
+            </p>
+          </div>
+          <div className="contact"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleResult;
